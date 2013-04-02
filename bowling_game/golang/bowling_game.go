9165ab646849ca2833c47b99aac8ef6847e40259
Timon Vonk <timonv@gmail.com>
2013-04-02 21:28:52 +0200

    Introduce simple frame

--- bowling_game/golang/bowling_game.go
+++ bowling_game/golang/bowling_game.go
@@ -3,20 +3,83 @@ package main
 import (
   "strconv"
   "strings"
-  "fmt"
+  //"fmt"
 )
 
 
-type Game string
+type Game struct {
+  rawScore string
+}
+
+type Frame struct {
+  rawScore []string
+}
+
+func NewGame(scr string) *Game {
+  game := new(Game)
+  game.rawScore = scr
+  return game
+}
 
 func (g *Game) Score() int {
-  arr := strings.Split(string(*g), "")
-  fmt.Println(arr)
   score := 0
-  for _, frame := range arr {
-    tmp, _ := strconv.ParseInt(frame, 10, 0)
-    score = score + int(tmp)
+  for _, val := range g.getScoreList() {
+    score += val
   }
 
   return score
+}
+
+func (g *Game) getScoreList() []int {
+  var list []int
+  split := g.splitScore()
+  for i, frame := range split {
+    var val int
+    if frame == "/" {
+      tmp, _ := strconv.ParseInt(split[i-1], 10, 0)
+      val = 10 - int(tmp)
+    } else {
+      tmp, _ := strconv.ParseInt(frame, 10, 0)
+      val = int(tmp)
+    }
+    list = append(list, val)
+  }
+  return list
+}
+
+func (g *Game) splitScore() []string {
+  return strings.Split(g.rawScore, "")
+}
+
+// Frames
+
+func NewFrame(scr []string) *Frame {
+  fr := new(Frame)
+  fr.rawScore = scr
+  return fr
+}
+
+func (fr *Frame) Score() int {
+  sum := 0
+  for _, val := range fr.getNormalizedScoreList() {
+    sum += val
+  }
+  return sum
+}
+
+func (fr *Frame) getNormalizedScoreList() []int {
+  var list []int
+  split := fr.rawScore
+  for i, frame := range split {
+    var val int
+    if frame == "/" {
+      tmp, _ := strconv.ParseInt(split[i-1], 10, 0)
+      val = 10 - int(tmp)
+    } else {
+      tmp, _ := strconv.ParseInt(frame, 10, 0)
+      val = int(tmp)
+    }
+    list = append(list, val)
+  }
+  return list
 }
--- bowling_game/golang/bowling_game_test.go
+++ bowling_game/golang/bowling_game_test.go
@@ -2,27 +2,42 @@ package main
 
 import (
   "testing"
-  "fmt"
+  //"fmt"
 )
 
-func TestTruth(t *testing.T) {
-  if false {
-    t.Fail()
+func assertScore(t *testing.T, score string, expected int) {
+  game := NewGame(score)
+  if game.Score() != expected {
+    t.Errorf("Game score was not %d, but %d", expected, game.Score())
   }
-  fmt.Println("GOGOGO")
 }
 
+func assertFrame(t *testing.T, score []string, expected int) {
+  fr := NewFrame(score)
+  if fr.Score() != expected {
+    t.Errorf("Frame score was not %d, but %d", expected, fr.Score())
+  }
+}
+
+// Integration
+
 func TestSimpleScores(t *testing.T) {
-  game1 := Game("000")
-  if game1.Score() != 0 {
-    t.Error("Score was not 0")
-  }
-  game2 := Game("111")
-  if game2.Score() != 3 {
-    t.Error("Score was not 3 but ", game2.Score())
-  }
-  game3 := Game("113")
-  if game3.Score() != 5 {
-    t.Error("Score was not 5 but ", game3.Score())
-  }
+  assertScore(t, "000", 0)
+  assertScore(t, "111", 3)
+  assertScore(t, "113", 5)
+}
+
+func TestMiss(t *testing.T) {
+  assertScore(t, "--1", 1)
+}
+
+func TestSpare(t *testing.T) {
+  assertScore(t, "1/00", 10)
+  //assertScore(t, "1/10", 12)
+}
+
+// Frame
+func TestFrameSimpleScore(t *testing.T) {
+  assertFrame(t, []string {"1", "0"}, 1)
+  assertFrame(t, []string {"5", "4"}, 9)
 }
